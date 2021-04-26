9b3b2c7dc7d77427af19a754f149bd9be5d8cbb7
Mielke44 <wilson.mielke@gmail.com>
2021-04-26 11:21:22 -0300

    Adição de método para subir Pokémon de level e evoluí-los

--- src/app/caught-pokemon-edit/caught-pokemon-edit.component.ts
+++ src/app/caught-pokemon-edit/caught-pokemon-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
 	selector: 'app-caught-pokemon-edit',
@@ -9,11 +10,42 @@ export class CaughtPokemonEditComponent implements OnInit {
 
 	@Input() pokemon_chosen;
 
-	constructor() {
+	constructor(private alertCtrl: AlertController) {
 	}
 
 	ngOnInit() {
 
+	}
+
+	public levelUp() {
+		if (this.pokemon_chosen.level < 100) {
+			this.pokemon_chosen.level++;
+		}
+	}
+
+	public async confirmEvolution() {
+		const alert = await this.alertCtrl.create({
+			header: 'Evolução!',
+			message: 'Você deseja mesmo evoluir esse Pokémon?',
+			buttons: [
+				{
+					text: 'Sim',
+					handler: () => this.evolvePokemon()
+				},
+				'Não'
+			]
+		});
+		alert.present();
+	}
+
+	public evolvePokemon() {
+		if(this.pokemon_chosen.id == 5){
+			this.pokemon_chosen = this.evolvingPokemon.find(pokemon => pokemon.id == 6);
+		} else if(this.pokemon_chosen.id == 90){
+			this.pokemon_chosen = this.evolvingPokemon.find(pokemon => pokemon.id == 91);
+		} else {
+			this.pokemon_chosen = this.evolvingPokemon.find(pokemon => pokemon.id == 87);
+		}
 	}
 
 	public evolvingPokemon = [
