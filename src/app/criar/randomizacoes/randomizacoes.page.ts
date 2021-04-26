1bc7a2ce6af861aba9b696a9c4297efdaaeb6243
Laura <laura_inocencio@outlook.com>
2021-04-25 22:38:25 -0300

    Implementação da tela de escolha de regra para randomizações

--- src/app/criar/randomizacoes/randomizacoes.page.ts
+++ src/app/criar/randomizacoes/randomizacoes.page.ts
@@ -11,5 +11,23 @@ export class RandomizacoesPage implements OnInit {
 
   ngOnInit() {
   }
+  public category = {
+		name: 'Randomizações',
+		description: null,
+		options: [
+			{
+				name: 'Pokémon randomizados?',
+				selected: false
+			},
+			{
+				name: 'Itens randomizados?',
+				selected: false
+			}
+		]
+	};
 
+	public printOption(){
+		console.log('Pokémon: ' + this.category.options[0].selected);
+		console.log('Itens: ' + this.category.options[1].selected);
+	}
 }
