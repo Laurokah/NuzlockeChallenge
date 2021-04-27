7efba47afde83aa332a08b9dc629ddcea65aa14e
MichelleNascimento <21186685@anhembimorumbi.edu.br>
2021-04-26 22:41:57 -0300

    Implementação de tela de exibição das regras selecionadas

--- src/app/nuzlocke/rules/rules.page.ts
+++ src/app/nuzlocke/rules/rules.page.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-rules',
-  templateUrl: './rules.page.html',
-  styleUrls: ['./rules.page.scss'],
+	selector: 'app-rules',
+	templateUrl: './rules.page.html',
+	styleUrls: ['./rules.page.scss'],
 })
 export class RulesPage implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+	
+	public rules = [
+		{
+			title: 'Captura',
+			description: 'Capturar obrigatoriamente o segundo Pokémon que aparecer'
+		},
+		{
+			title: 'Recaptura',
+			description: 'O encontro na rota é considerado perdido'
+		},
+		{
+			title: 'Morte',
+			description: 'Se desmaiar apenas em batalhas contra treinadores'
+		},
+		{
+			title: 'Reviver',
+			description: 'A cada ginásio vencido, o jogador pode reviver um dos Pokémon mortos'
+		},
+		{
+			title: 'Ginásios',
+			description: 'Os Pokémon devem estar a 2 níveis abaixo do Pokémon mais forte do líder'
+		},
+		{
+			title: 'Randomização',
+			description: 'Apenas Pokémon randomizados'
+		}
+	]
 
 }
