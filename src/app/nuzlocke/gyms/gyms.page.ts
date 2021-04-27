48f7b932b92621d3175bf24e70484014b4b1e81d
MichelleNascimento <21186685@anhembimorumbi.edu.br>
2021-04-26 22:32:32 -0300

    Adição de lógica para marcar o ginásio como concluído e modificar ícone correspondente

--- src/app/nuzlocke/gyms/gyms.page.ts
+++ src/app/nuzlocke/gyms/gyms.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
 	selector: 'app-gyms',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GymsPage implements OnInit {
 
-	constructor() { }
+	constructor(private alertCtrl: AlertController) { }
 
 	ngOnInit() {
 	}
@@ -15,43 +16,71 @@ export class GymsPage implements OnInit {
 	public badges = [
 		{
 			number: 1,
-			iconSource: './assets/images/gyms/kanto-1.png',
+			iconSource: './assets/images/gyms/kanto-1-unchecked.png',
 			completed: false
 		},
 		{
 			number: 2,
-			iconSource: './assets/images/gyms/kanto-2.png',
+			iconSource: './assets/images/gyms/kanto-2-unchecked.png',
 			completed: false
 		},
 		{
 			number: 3,
-			iconSource: './assets/images/gyms/kanto-3.png',
+			iconSource: './assets/images/gyms/kanto-3-unchecked.png',
 			completed: false
 		},
 		{
 			number: 4,
-			iconSource: './assets/images/gyms/kanto-4.png',
+			iconSource: './assets/images/gyms/kanto-4-unchecked.png',
 			completed: false
 		},
 		{
 			number: 5,
-			iconSource: './assets/images/gyms/kanto-5.png',
+			iconSource: './assets/images/gyms/kanto-5-unchecked.png',
 			completed: false
 		},
 		{
 			number: 6,
-			iconSource: './assets/images/gyms/kanto-6.png',
+			iconSource: './assets/images/gyms/kanto-6-unchecked.png',
 			completed: false
 		},
 		{
 			number: 7,
-			iconSource: './assets/images/gyms/kanto-7.png',
+			iconSource: './assets/images/gyms/kanto-7-unchecked.png',
 			completed: false
 		},
 		{
 			number: 8,
-			iconSource: './assets/images/gyms/kanto-8.png',
+			iconSource: './assets/images/gyms/kanto-8-unchecked.png',
 			completed: false
 		}
 	];
+
+	public selectedBadgeNumber = 0;
+
+	public async confirmGymCompletion(badgeNumber: number) {
+		this.selectedBadgeNumber = badgeNumber;
+		const alert = await this.alertCtrl.create({
+			header: 'Concluir ginásio',
+			message: 'Você deseja mesmo marcar esse ginásio como concluído?',
+			buttons: [
+				{
+					text: 'Sim',
+					handler: () => this.markGymAsConcluded()
+				},
+				'Não'
+			]
+		});
+		alert.present();
+	}
+
+	public markGymAsConcluded() {
+		let concludedBadge = this.badges.find(
+			badge => badge.number == this.selectedBadgeNumber
+		);
+		concludedBadge.iconSource = concludedBadge.iconSource.substring(0, 28) + '.png';
+		concludedBadge.completed = true;
+
+		console.log('Ginásio nº ' + concludedBadge.number + ' concluído');
+	}
 }
