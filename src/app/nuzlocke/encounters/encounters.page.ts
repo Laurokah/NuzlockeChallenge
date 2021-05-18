ccc434b1d6900c89357bfeab9ff674a964d50e72
Lucas Freire <hey-lucas@hotmail.com>
2021-05-17 22:42:15 -0300

    Implementação do registro de encontros nas rotas

--- src/app/nuzlocke/encounters/encounters.page.ts
+++ src/app/nuzlocke/encounters/encounters.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController} from '@ionic/angular';
+import { CaptureService } from 'src/app/services/capture.service';
+import { ChosenRulesService } from 'src/app/services/chosen-rules.service';
 @Component({
 	selector: 'app-encounters',
 	templateUrl: './encounters.page.html',
@@ -8,125 +10,41 @@ import { AlertController} from '@ionic/angular';
 })
 export class EncountersPage implements OnInit {
 
-	constructor(private alertCtrl: AlertController, private router: Router) { }
+	constructor(
+		private alertCtrl: AlertController,
+		private router: Router,
+		public captureService: CaptureService,
+		public chosenRulesService: ChosenRulesService
+	){}
 
 	ngOnInit() {
 	}
 
-	public routes = [
-		{
-			name: 'Route 1',
-			waiting: false,
-			successful: true,
-			iconName: 'checkmark',
-			pokemonSpecies: 'Staryu',
-			pokemonNickname: 'Misty',
-			pokemonLevel: 15,
-			pokemon: {
-				id: 120,
-				iconSource: './assets/images/icons/120.png'
-			}
-		},
-		{
-			name: 'Route 2',
-			waiting: false,
-			successful: false,
-			iconName: 'close',
-			pokemonSpecies: '',
-			pokemonNickname: '',
-			pokemonLevel: 0
-		},
-		{
-			name: 'Route 3',
-			waiting: false,
-			successful: true,
-			iconName: 'checkmark',
-			pokemonSpecies: 'Electabuzz',
-			pokemonNickname: '',
-			pokemonLevel: 19,
-			pokemon: {
-				id: 125,
-				iconSource: './assets/images/icons/125.png'
-			}
-		},
-		{
-			name: 'Route 4',
-			waiting: true,
-			successful: false,
-			iconName: 'time-outline',
-			pokemonSpecies: '',
-			pokemonNickname: '',
-			pokemonLevel: 0
-		},
-		{
-			name: 'Route 5',
-			waiting: true,
-			successful: false,
-			iconName: 'time-outline',
-			pokemonSpecies: '',
-			pokemonNickname: '',
-			pokemonLevel: 0
-		},
-		{
-			name: 'Route 6',
-			waiting: true,
-			successful: false,
-			iconName: 'time-outline',
-			pokemonSpecies: '',
-			pokemonNickname: '',
-			pokemonLevel: 0
-		},
-		{
-			name: 'Route 7',
-			waiting: true,
-			successful: false,
-			iconName: 'time-outline',
-			pokemonSpecies: '',
-			pokemonNickname: '',
-			pokemonLevel: 0
-		},
-		{
-			name: 'Route 8',
-			waiting: true,
-			successful: false,
-			iconName: 'time-outline',
-			pokemonSpecies: '',
-			pokemonNickname: '',
-			pokemonLevel: 0
-		},
-		{
-			name: 'Route 9',
-			waiting: true,
-			successful: false,
-			iconName: 'time-outline',
-			pokemonSpecies: '',
-			pokemonNickname: '',
-			pokemonLevel: 0
-		},
-		{
-			name: 'Route 10',
-			waiting: true,
-			successful: false,
-			iconName: 'time-outline',
-			pokemonSpecies: '',
-			pokemonNickname: '',
-			pokemonLevel: 0
-		}
-	];
+	public makeNothing(){}
 
-	public makeNothing() { }
-	public async hasEncounterFailed(routeIndex: number) {
+	public registerEncounter(routeIndex: number){
+		if(	
+			routeIndex != 0 &&
+			this.chosenRulesService.chosenRecaptureRule.description == "O encontro na rota é considerado perdido"
+		){
+			this.hasEncounterFailed(routeIndex);
+		} else {
+			this.registerSuccess(true, routeIndex);
+	}
+}
+
+	public async hasEncounterFailed(routeIndex: number){
 		const alert = await this.alertCtrl.create({
 			header: 'Registrar encontro',
 			message: 'Você capturou o Pokémon nessa rota?',
 			buttons: [
 				{
 					text: 'Sim',
-					handler: () => this.registerSuccessfulEncounter(routeIndex)
+					handler: () => this.registerSuccess(true, routeIndex)
 				},
 				{
 					text: 'Não',
-					handler: () => this.changeEncounterStatus(routeIndex, false)
+					handler: () => this.registerSuccess(false, routeIndex)
 				},
 				'Voltar'
 			]
@@ -134,20 +52,12 @@ export class EncountersPage implements OnInit {
 		alert.present();
 	}
 
-	public registerSuccessfulEncounter(routeIndex: number) {
-		this.router.navigate(['/nuzlocke/encounters/register']);
-		this.changeEncounterStatus(routeIndex, true)
-	}
-
-	public changeEncounterStatus(routeIndex: number, successful: boolean) {
-		if (successful) {
-			this.routes[routeIndex].iconName = 'checkmark';
-			this.routes[routeIndex].successful = true;
+	public registerSuccess(isSuccess, routeIndex){
+		if(isSuccess){
+			this.router.navigate(['/nuzlocke/encounters/register']);
+			this.captureService.routeIndex = routeIndex;
 		} else {
-			this.routes[routeIndex].iconName = 'close';
-			this.routes[routeIndex].successful = false;
+			this.captureService.changeEncounterStatus(routeIndex, false)
 		}
-		this.routes[routeIndex].waiting = false;
-		console.log('Houve encontro na rota ' + this.routes[routeIndex].name + ': ' + this.routes[routeIndex].successful);
 	}
 }
---
+++ src/app/services/capture.service.ts
@@ -0,0 +1,126 @@
+import { Injectable } from '@angular/core';
+import { PokemonDatabaseService } from './pokemon-database.service';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class CaptureService {
+
+	constructor(
+		public pokemonDatabaseService: PokemonDatabaseService
+	){}
+
+	public routeIndex;
+
+	public currentRoute = {
+		name: null,
+		waiting: true,
+		successful: false,
+		iconName: null,
+		level: null,
+		nickname: null,
+		species: null,
+		iconSource: null
+	};
+
+	public kantoRoutes = [
+		'Inicial',
+		'Route 1',
+		'Route 2',
+		'Route 3',
+		'Route 4',
+		'Route 4 (gift)',
+		'Route 5',
+		'Route 6',
+		'Route 7',
+		'Route 8',
+		'Route 9',
+		'Route 10',
+		'Route 11',
+		'Route 12',
+		'Route 12 (static)',
+		'Route 13',
+		'Route 14',
+		'Route 15',
+		'Route 16',
+		'Route 16 (static)',
+		'Route 17',
+		'Route 18',
+		'Route 19',
+		'Route 20',
+		'Route 21',
+		'Route 22',
+		'Route 23',
+		'Route 24',
+		'Route 25',
+		'Cerulean Cave',
+		'Cerulean Cave (static)',
+		'Cerulean City',
+		'Cinnabar Island',
+		'Celadon City',
+		'Celadon Mansion (gift)',
+		'Digglet\'s Cave',
+		'Fuchsia City',
+		'Fighting Dojo (gift)',
+		'Pewter Museum Fossil (gift)',
+		'Silph Co. (gift)',
+		'Mt. Moon',
+		'Mt. Moon Fossil (gift)',
+		'Pallet Town',
+		'Pokémon Mansion',
+		'Pokémon Tower',
+		'Power Plant'		,
+		'Power Plant (static)',
+		'Rock Tunnel',
+		'Safari Zone',
+		'Seafoam Islands',
+		'Seafoam Islands (static)',
+		'SS Anne Harbor',
+		'Vermilion City',
+		'Victory Road',
+		'Victory Road (static)',
+		'Viridian City',
+		'Viridian Forest',
+	];
+
+	public routes = this.clone(1);
+
+	public clone(gameGen){
+		let routesToCopy = [];
+		for (const routeName of this.kantoRoutes){
+			routesToCopy.push({
+				name: routeName,
+				waiting: true,
+				successful: false,
+				iconName: 'time-outline',
+				level: null,
+				nickname: null,
+				species: null,
+				iconSource: null
+			});
+		}
+		return routesToCopy;
+	}
+
+	public registerPokemon(savedPokemon){
+		this.changeEncounterStatus(this.routeIndex, true)
+
+		this.currentRoute.level = savedPokemon.level;
+		this.currentRoute.nickname = savedPokemon.nickname;
+		this.currentRoute.species = savedPokemon.pokemon.species;
+		this.currentRoute.iconSource = savedPokemon.pokemon.iconSource;
+	}
+
+	public changeEncounterStatus(routeIndex: number, successful: boolean) {
+		this.currentRoute = this.routes[routeIndex];
+		if (successful) {
+			this.currentRoute.iconName = 'checkmark';
+			this.currentRoute.successful = true;
+		} else {
+			this.currentRoute.iconName = 'close';
+			this.currentRoute.successful = false;
+		}
+		this.currentRoute.waiting = false;
+	}
+
+}
