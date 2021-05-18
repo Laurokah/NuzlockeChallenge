70a4dc1d55251b87c1399fc63ad297f8afcfa277
Lucas Freire <hey-lucas@hotmail.com>
2021-05-17 22:53:33 -0300

    Validações para tornar obrigatórias as inserções de level e espécie do pokémon capturado

--- src/app/nuzlocke/encounters/register/register.page.ts
+++ src/app/nuzlocke/encounters/register/register.page.ts
@@ -3,7 +3,6 @@ import { Router } from '@angular/router';
 import { CaptureService } from 'src/app/services/capture.service';
 import { PokemonDatabaseService } from 'src/app/services/pokemon-database.service';
 
-
 @Component({
 	selector: 'app-register',
 	templateUrl: './register.page.html',
@@ -20,21 +19,49 @@ export class RegisterPage implements OnInit {
 	ngOnInit() {
 	}
 
+	public invalid = false;
+	public errorMessage;
+
 	public chosenDataPokemon;
 	public pokemonNickname;
 	public pokemonLevel;
 
 	public saveEncounter(){
-		let savedPokemon = {
-			url: null,
-			status: null,
-			nickname: this.pokemonNickname,
-			level: this.pokemonLevel,
-			pokemon: this.chosenDataPokemon
-		};
-		this.captureService.registerPokemon(savedPokemon);
-		
-		this.router.navigate(['/nuzlocke/encounters']);
+		if(!this.isInputInvalid()){
+			this.invalid = false;
+			let savedPokemon = {
+				url: null,
+				status: null,
+				nickname: this.pokemonNickname,
+				level: this.pokemonLevel,
+				pokemon: this.chosenDataPokemon
+			};
+			this.captureService.registerPokemon(savedPokemon);
+			
+			this.router.navigate(['/nuzlocke/encounters']);
+		} else {
+			this.invalid = true;
+		}
+	}
 
+	public isInputInvalid(){
+		if(this.pokemonLevel != null){
+			if(this.pokemonLevel <= 0 || this.pokemonLevel > 100){
+				this.errorMessage = 'O level do Pokémon deve estar entre 1 e 100';
+			} else if(this.chosenDataPokemon == null){
+				this.errorMessage = 'Você deve inserir qual o Pokémon capturado';
+			} else {
+				return false;
+			}
+		} else {
+			if(this.chosenDataPokemon == null){
+				this.errorMessage = 'Você deve inserir qual o Pokémon capturado e o seu level';
+			} else {
+				this.errorMessage = 'Você deve inserir qual o level do Pokémon capturado';
+			}
+			return true;
+		}
+
+		return true;
 	}
 }
