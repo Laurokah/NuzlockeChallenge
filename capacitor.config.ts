62225e2a83103e789f4f689cca90c6b7f8de5b02
MichelleNascimento <21186685@anhembimorumbi.edu.br>
2021-06-03 22:35:39 -0300

    Adição de Android para geração do APK

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.anhembi.nuzlockecreator',
+  appName: 'Nuzlocke Creator',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
